241837f3a72997d5c1f54bbc36a6234d2ac9cf2a
KaianeSousa <132933423+KaianeSousa@users.noreply.github.com>
2023-11-04 11:39:45 -0300

    Create sgc.h
    biblioteca para a implementação de um sistema de gestão comercial

---
+++ sgc.h
@@ -0,0 +1,54 @@
+#ifndef SGC_H
+#define SGC_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_NAME_USER 1500
+#define MAX_TAM_PASS 100
+#define MAX_PRODUTOS 1100
+#define MAX_NAME_PROD 1100
+
+    enum tipoUsuario{
+        ADMINISTRADOR, USER_CONVENCIONAL
+    };
+    
+    struct Produto{
+        char nomeProduto[MAX_NAME_PROD];
+        int idProduto;
+        int quantidade;
+        float preco;
+        
+    };
+    
+    struct Usuario{
+        char nomeUsuario[MAX_NAME_USER];
+        int idUsuario;
+        char senha[MAX_TAM_PASS];
+        enum tipoUsuario tipoUsuario;
+    };
+
+//Funções da main
+    void mostrarMenu();
+    void interacao();
+    void menuAdmin(struct Usuario *usuario, struct Produto *produto, int tentativas);
+    void menuUsuario(struct Usuario *usuario, struct Produto *produto, int tam_estoque, int tam_lista);
+    
+//Funções do usuário Administrador
+
+    void interacaoAdmin(struct Usuario *usuario, struct Produto *produto, int tam_estoque, int tam_lista);
+    void adicionarProduto(struct Produto *produto, int *tam_estoque);
+    void listarProdutos(struct Produto *produto, int tam_estoque);
+    void cadastrarUsuario(struct Usuario *usuario, int *tam_lista);
+    void excluirUsuario(struct Usuario *usuario, int *tam_lista);
+    void mostrarDados(struct Produto *produto, int *tam_estoque, struct Usuario *usuario, int *tam_lista);
+
+
+//Funções do usuário Convencional 
+
+    void interacaoUsuario(struct Produto *produto, struct Usuario *usuario, int *tam_estoque, int *tam_lista);
+    void pedidos(struct Produto *produto, int *tamanho);
+    void listarPedidos();
+
+#endif
